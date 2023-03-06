c680b032dc673eab0379858a9d6854833eabd8f8
BondPV <bondpv@yandex.ru>
2023-03-06 10:43:12 +0300

    feat: add server url

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,9 +1,9 @@
-const BASE_URL = 'http://127.0.0.1:3000';
+const BASE_URL = 'https://async-race-tsqf.onrender.com';
 export const GARAGE_URL = `${BASE_URL}/garage`;
 export const ENGINE_URL = `${BASE_URL}/engine`;
 export const WINNERS_URL = `${BASE_URL}/winners`;
 
 export const CARS_PER_PAGE = 7;
 export const WINNERS_PER_PAGE = 10;
-export const DEFAULT_RANDOM_COUNT = 100;
+export const DEFAULT_RANDOM_COUNT = 5;
 export const MODAL_WINDOW_TIMEOUT = 2000;
